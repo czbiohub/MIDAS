b16a854257fbbbdffefd1d1807bf54298eaeda9f
Chunyu Zhao <zhaocy.dut@gmail.com>
2019-06-28 22:27:06 -0700

    Autosave commit created on Fri Jun 28 22:27:06 PDT 2019.

--- smelter/main.py
+++ smelter/main.py
@@ -87,6 +87,10 @@ def smelt(argv):
                 s_repgenome_with_origin = s['repgenome_with_origin']
                 s_repgenome_path = s['repgenome_path']
                 s_header_xform = f"sed 's=^>=>{s_species_alt_id}|{s_repgenome_with_origin}|=' {s_repgenome_path} > {s_tempfile} && cat {s_tempfile} >> {outdir}/temp_{gdim}.fa && rm {s_tempfile} && echo SUCCEEDED || echo FAILED"
+
+                s_repgenome_path_clean = s['repgenome_path_clean']
+                s_header_xform = f"sed 's=^>=>{s_species_alt_id}|=' {s_repgenome_path} > {s_repgenome_path_clean} && cat {s_repgenome_path_clean} >> {outdir}/temp_{gdim}.fa && echo SUCCEEDED || echo FAILED"
+
             status = backtick(s_header_xform)
             assert status == "SUCCEEDED"
             count_successes += 1
