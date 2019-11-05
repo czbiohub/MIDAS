6d8943ba8ac62b3588d02a1c0de9fee894dcf1c5
Chunyu Zhao <zhaocy.dut@gmail.com>
2019-11-05 00:11:59 -0800

    Autosave commit created on Tue Nov  5 00:11:59 PST 2019.

--- midas/run/species.py
+++ midas/run/species.py
@@ -139,9 +139,11 @@ def get_markers(args):
 
 def read_gene_lengths(args, species_info, marker_info):
 	""" Read in total gene length per species_id """
+	print("read_gene_lengths:", species_info)
 	total_gene_length = dict([(_,0) for _ in species_info])
 	for r in marker_info.values():
 		total_gene_length[r['species_id']] += int(r['gene_length'])
+	print("read_gene_lengths:", total_gene_length)
 	return total_gene_length
 
 def normalize_counts(species_alns, total_gene_length):
@@ -200,7 +202,8 @@ def select_species(args):
 	species_sets = {}
 	# read in species abundance if necessary
 	if any([args['species_topn'], args['species_cov']]) and not args['all_species_in_db']:
-		species_abundance = read_abundance('%s/iggsearch/species_profile.tsv' % args['outdir'])
+		## IGGsearch results
+		#species_abundance = read_abundance('%s/iggsearch/species_profile.tsv' % args['outdir'])
 		species_abundance = read_abundance('%s/species/species_profile.tsv' % args['outdir'])
 		# user specifed a coverage threshold
 		if args['species_cov']:
@@ -265,7 +268,7 @@ def run_pipeline(args):
 	start = time()
 	print("\nAligning reads to marker-genes database")
 	args['log'].write("\nAligning reads to marker-genes database\n")
-	map_reads_hsblast(args)
+	#map_reads_hsblast(args)
 	print("  %s minutes" % round((time() - start)/60, 2))
 	print("  %s Gb maximum memory" % utility.max_mem_usage())
 
