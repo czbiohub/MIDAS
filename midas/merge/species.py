9c38ddd52423c85732c6569df0092ed4992f6f25
Chunyu Zhao <zhaocy.dut@gmail.com>
2019-12-02 15:16:51 -0800

    Autosave commit created on Mon Dec  2 15:16:51 PST 2019.

--- midas/merge/species.py
+++ midas/merge/species.py
@@ -13,13 +13,13 @@ class Sample:
 	def __init__(self, dir):
 		self.dir = dir
 		self.id = os.path.basename(self.dir)
-		self.path = '%s/results/species/species_profile.txt' % self.dir
+		self.path = '%s/species/species_profile.txt' % self.dir
 
 def init_samples(indirs):
 	""" Initialize samples """
 	samples = []
 	for sample_dir in indirs:
-		species_profile = '%s/results/species/species_profile.txt' % sample_dir
+		species_profile = '%s/species/species_profile.txt' % sample_dir
 		if not os.path.exists(species_profile):
 			sys.stderr.write("Warning: missing/incomplete output: %s\n" % sample_dir)
 		else:
